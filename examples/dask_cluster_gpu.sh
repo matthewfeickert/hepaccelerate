339a59a28d927a7f3882728a9dd80cdea7168867
Joosep Pata <joosep.pata@cern.ch>
2020-01-02 14:54:44 -0800

    up

--- examples/dask_cluster_gpu.sh
+++ examples/dask_cluster_gpu.sh
@@ -4,8 +4,8 @@ dask-scheduler &
 
 sleep 10
 
-for idev in `seq 0 1`; do
-    for iprl in `seq 0 2`; do
+for idev in `seq 0 7`; do
+    for iprl in `seq 0 1`; do
         CUDA_VISIBLE_DEVICES=$idev PYTHONPATH=.:examples dask-worker 127.0.0.1:8786 --no-nanny --nthreads 1 --nprocs 1 --memory-limit 0 &
     done
 done
